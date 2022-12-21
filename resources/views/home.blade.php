d060d3512420b65db6146c1d4d5ff0c6f1f95a1f
balqisKhairi <balqiskhairi19@gmail.com>
2022-12-21 09:11:25 +0800

    21.12.2022

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Hye {{ Auth::user()->name }}, welcome back!  {{ __('You are logged in') }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
